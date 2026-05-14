05c6bd7ce9a431a7c9a12939daea4b5f25c8e779
Ardhaya Johari <ardhayasaxena3897@gmail.com>
2026-05-14 19:59:31 +0530

    Remove FilterBar component implementation

--- src/components/transactions/FilterBar.jsx
+++ src/components/transactions/FilterBar.jsx
@@ -1,19 +1,4 @@
 import { useApp } from "../../context/AppContext";
 
-const FilterBar = () => {
-  const { search, setSearch } = useApp();
-
-  return (
-    <div className="flex gap-4 mb-4">
-      <input
-        type="text"
-        placeholder="Search category..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="p-2 rounded bg-white/10 border border-white/20 text-white"
-      />
-    </div>
-  );
-};
 
 export default FilterBar;
